7bc257e0668dad7ac01eba07edc4e2f667952aca
Diogo Rosa <a50533@alunos.isel.pt>
2025-03-12 17:09:18 +0000

    first commit

---
+++ code/src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package isel.openapi.mock
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
